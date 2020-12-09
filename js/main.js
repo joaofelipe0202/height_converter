126dcd0e5da6d999e634d345ce25a7158a77bd48
Joao Felipe - Johnny <joaobarbo@student.mitt.ca>
2020-12-09 02:30:03 -0300

    create an event listener to submit the values

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ converter.addEventListener('submit', event => {
   let height = document.querySelector('.height');
   const feetValue = document.querySelector('.feet');
   const inchesValue = document.querySelector('.inches');
-
+  event.preventDefault();
   height = parseInt(height.value);
 
-  if (inNaN(height) || (height.value = '')) {
+  if (isNaN(height) || (height.value = '')) {
     alert('Only valid numbers, please');
   } else if (height <= 0) {
     alert('Only values greater than ZERO!');
@@ -26,9 +26,9 @@ converter.addEventListener('submit', event => {
 
 
 
-let number = 175;
+/*let number = 175;
 let feet = number*0.0328084;
 
 let decimal = feet - Math.trunc(feet);
 let inches = Math.ceil(decimal*12)
-console.log(`${Math.trunc(feet)}' ${inches}"`)
+console.log(`${Math.trunc(feet)}' ${inches}"`)*/
