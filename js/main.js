e274cbb28c19090416f1413c25f31bbbdbaef998
Joao Felipe - Johnny <joaobarbo@student.mitt.ca>
2020-12-09 02:22:04 -0300

    add code to js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+const converter = document.querySelector('.converter');
+
+converter.addEventListener('submit', event => {
+  let height = document.querySelector('.height');
+  const feetValue = document.querySelector('.feet');
+  const inchesValue = document.querySelector('.inches');
+
+  height = parseInt(height.value);
+
+  if (inNaN(height) || (height.value = '')) {
+    alert('Only valid numbers, please');
+  } else if (height <= 0) {
+    alert('Only values greater than ZERO!');
+  } else {
+    let feet = height*0.0328084;
+    let decimal = feet - Math.trunc(feet);
+    let inches = Math.ceil(decimal*12);
+
+    feetValue.value = `${Math.trunc(feet)}'`;
+    inchesValue.value = `${inches}"`;
+  }
+})
+
+
+
+
+
+
 let number = 175;
 let feet = number*0.0328084;
 
